2ff1523e499a94dc6aa5da7012aaffa3f3eaec62
Shanshan (Shanshan) He <sh2df@cuda1.cs.virginia.edu>
2015-04-16 01:04:21 -0400

    Problem 4 solved

--- ARCH_ASSIGNMENT/vector_max.cu
+++ ARCH_ASSIGNMENT/vector_max.cu
@@ -190,7 +190,59 @@ __global__ void vector_max_kernel3(float *in, float *out, int N) {
     }
         out[block_id] = share[0];
 
+}
+
+__global__ void vector_max_kernel4(float *in, float *out, int N) {
+
+    // Determine the "flattened" block id and thread id
+    int block_id = blockIdx.x + gridDim.x * blockIdx.y;
+    int thread_id = blockDim.x * block_id + threadIdx.x;
+    __shared__ float share[256];
+    share[threadIdx.x] = in[thread_id];
+    __syncthreads();
+	int end = threads_per_block;
+
+	if (threadIdx.x == 0) {
+        	//calculate out of bounds guard
+        	//our block size will be 256, but our vector may not be a multiple of 256!
+       		if(thread_id + threads_per_block > N)
+            	end = N - thread_id;
+	}
+    // A single "lead" thread in each block finds the maximum value over a range of size threads_per_block
+    float max = 0.0;
+    int round;
+    round = log2(double(threads_per_block));
+    //    cuPrintf("round is %d\n", round);
+    for(int i = 1; i < round + 1; i++) {
+      __syncthreads();
+	int step = (int)ceil(end/powf(2.0,i));
+      if (threadIdx.x < step) { 
+	//	cuPrintf("round is %d\n", i);
+
+        //calculate out of bounds guard
+        //our block size will be 256, but our vector may not be a multiple of 256!
+      // int end = threads_per_block;
+      // if(thread_id + threads_per_block > N)
+      //    end = N - thread_id;
+
+        //grab the lead thread's value
+        max = share[threadIdx.x];
+
+        //grab values from all other threads' locations
+        
+                
+            //if larger, replace
+	if( threadIdx.x + step < end )
+            if(max < share[threadIdx.x + step])
+                max = share[threadIdx.x + step];
+      
+	share[threadIdx.x] = max;
+      }
+      __syncthreads();
     }
+        out[block_id] = share[0];
+
+}
 
 
 // Returns the maximum value within a vector of length N
@@ -272,8 +324,6 @@ float GPU_vector_max(float *in_CPU, int N, int kernel_code) {
      cudaPrintfInit();
      vector_max_kernel3 <<< grid_size , threads_per_block >>> (in_GPU, out_GPU, N);
      do {
-       
-
        cudaMemcpy(in_GPU, out_GPU, N, cudaMemcpyDeviceToDevice);
 
        int numblocks = (int) ((float) (N + threads_per_block - 1) / (float) threads_per_block);
@@ -289,7 +339,19 @@ float GPU_vector_max(float *in_CPU, int N, int kernel_code) {
         break;
     case 4 :
         //LAUNCH KERNEL FROM PROBLEM 4 HERE
-        die("KERNEL NOT IMPLEMENTED YET\n");
+     vector_max_kernel3 <<< grid_size , threads_per_block >>> (in_GPU, out_GPU, N);
+     do {
+       cudaMemcpy(in_GPU, out_GPU, N, cudaMemcpyDeviceToDevice);
+
+       int numblocks = (int) ((float) (N + threads_per_block - 1) / (float) threads_per_block);
+       int numblocks_y = (int) ((float) (numblocks + max_blocks_per_dimension - 1) / (float) max_blocks_per_dimension);
+       int numblocks_x = (int) ((float) (numblocks + numblocks_y - 1) / (float) numblocks_y);
+       dim3 grid_size2(numblocks_x, numblocks_y, 1);
+       N = numblocks;
+       vector_max_kernel4 <<< grid_size2 , threads_per_block >>> (in_GPU, out_GPU, N);
+
+     } while(N > 1);
+        //die("KERNEL NOT IMPLEMENTED YET\n");
         break;
     default :
         die("INVALID KERNEL CODE\n");
