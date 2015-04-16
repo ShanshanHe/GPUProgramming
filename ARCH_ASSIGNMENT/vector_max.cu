d726e2611b595b786ce29f3277e6778ee7c64165
Luonan Wang <lw3mz@cuda1.cs.virginia.edu>
2015-04-15 21:36:09 -0400

    Problem 1: computing the maxium of a vector has been done; Problem 2: Shared memory used in kenel

--- ARCH_ASSIGNMENT/vector_max.cu
+++ ARCH_ASSIGNMENT/vector_max.cu
@@ -116,6 +116,52 @@ __global__ void vector_max_kernel(float *in, float *out, int N) {
     }
 }
 
+
+
+__global__ void vector_max_kernel2(float *in, float *out, int N) {
+
+    // Determine the "flattened" block id and thread id
+    int block_id = blockIdx.x + gridDim.x * blockIdx.y;
+    int thread_id = blockDim.x * block_id + threadIdx.x;
+    //cuPrintf("block_id = %d\n", block_id);
+    //cuPrintf("thread_id = %d\n", thread_id);
+    //cuPrintf("blockIdx.x = %d\n", blockIdx.x);
+    //cuPrintf("blockIdx.y = %d\n", blockIdx.y);
+    cuPrintf("threadIdx.x = %d\n", threadIdx.x);
+    //cuPrintf("threadIdx.y = %d\n", threadIdx.y);
+
+	_shared_ int sharemem[N];
+        sharemem = in; 
+
+    // A single "lead" thread in each block finds the maximum value over a range of size threads_per_block
+    float max = 0.0;
+    if (threadIdx.x == 0) {
+
+        //calculate out of bounds guard
+        //our block size will be 256, but our vector may not be a multiple of 256!
+        int end = threads_per_block;
+        if(thread_id + threads_per_block > N)
+            end = N - thread_id;
+
+        //grab the lead thread's value
+        max = sharemem[thread_id];
+
+        //grab values from all other threads' locations
+        for(int i = 1; i < end; i++) {
+
+            //if larger, replace
+            if(max < thread_id + i])
+                max = [thread_id + i];
+        }
+
+        out[block_id] = max;
+
+    }
+
+
+
+
+
 // Returns the maximum value within a vector of length N
 float GPU_vector_max(float *in_CPU, int N, int kernel_code) {
 
